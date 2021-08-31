44694b28e5240ac6be776944a9b5da9564d4c44c
purohitsumit <purohitsumit@gmail.com>
2021-08-30 20:52:18 -0700

    argument added

--- ITeM.sh
+++ ITeM.sh
@@ -1,5 +1,5 @@
 java -cp ./target/uber-STM-1.3-SNAPSHOT.jar gov.pnnl.stm.algorithms.STM_NodeArrivalRateMultiType -Dspark.master=local[*] \
--input_file="D:/localcode/STM/data/testGDifficult2.csv" -out_json_file_os_path="D:\localdata\mao_global_edges_ITeM_log2.json" -separator="," \
+-input_file=$1 -out_json_file_os_path="D:\localdata\mao_global_edges_ITeM_log2.json" -separator="," \
 -avg_outdeg_file="D:\localdata\mao_global_edges_ITeM_log2_deg.csv" \
 -sampling=false \
 -sampling_population=16 \
@@ -10,7 +10,7 @@ java -cp ./target/uber-STM-1.3-SNAPSHOT.jar gov.pnnl.stm.algorithms.STM_NodeArri
 -t_delta=600 \
 -max_cores=4 \
 -k_top=4 \
--base_out_dir=".\output\autorunTest\\"
+-base_out_dir=$2
 
 echo "ITeM Processing Done. Generating embeddings\n"
 
